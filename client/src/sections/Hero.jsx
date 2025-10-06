57c1a04df2c38f90b37aac90f203fab16bdcd79d
Clark Sean Payabyab <clarkpayabyab17@gmail.com>
2025-10-07 02:20:04 +0800

    updated navbar

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     scrollToSection(sectionId);
   };
   return (
-    <div id="hero-section" className="hero bg-background lg:h-screen">
+    <div id="hero-section" className="hero bg-background h-full mt-21">
       <div className="hero-content flex-col md:flex-row md:px-0 py-12 justify-around gap-0 w-full max-w-full">
         <div className="flex flex-col items-center">
           <img src={shirt1} alt="Shirt" className="w-60 lg:w-80 xl:w-120" />
